82dcf62e8108532dae04c317645edd5fe8b73c57
Raccoon2508 <ridge2006@mail.ru>
2022-03-08 01:26:22 +0300

    Add data models and data repository structure

---
+++ data.js
@@ -0,0 +1,16 @@
+module.exports = function() {
+    return {
+        products: [
+            { id: 1, name: 'Kayak', category: 'Watersports', description: "A boat blablabla", price: 275 },
+            { id: 2, name: 'LifeJacket', category: 'Watersports', description: "lablabla", price: 48.95 },
+            { id: 3, name: 'Soccer ball', category: 'Soccer', description: "FIFA approved", price: 19.50 },
+            { id: 4, name: 'Corner Flags', category: 'Soccer', description: "For playing field", price: 34.95 },
+            { id: 5, name: 'Stadium', category: 'Soccer', description: "Stadium", price: 79500 },
+            { id: 6, name: 'Unsteady Chair', category: 'Chess', description: "Improve efficient", price: 16 },
+            { id: 7, name: 'Human Chess Board', category: 'Chess', description: "A fun game for the family", price: 75 },
+            { id: 9, name: 'Bling Bling King', category: 'Chess', description: "King gold-plated", price: 1200 },
+
+        ],
+        orders: []
+    }
+}
