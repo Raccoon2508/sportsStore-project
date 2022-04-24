42c98df1797b8dc775f258bcd2cd0dfc9cfe597f
Raccoon2508 <ridge2006@mail.ru>
2022-04-24 18:05:32 +0300

    Update project struture, add stylelint and Husky script for it.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from './store/store.module';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
     ],
     imports: [
-        BrowserModule
+        BrowserModule, StoreModule
     ],
     providers: [],
-    bootstrap: [AppComponent]
+    bootstrap: [AppComponent],
 })
 export class AppModule { }
