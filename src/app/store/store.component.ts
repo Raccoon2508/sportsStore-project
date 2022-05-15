db8f590b0a42085107200a646d6abb327be144c6
Raccoon2508 <ridge2006@mail.ru>
2022-05-15 23:17:19 +0300

    Update home page (page 155)

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,21 +1,64 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Product } from '@app/model/product.model';
 import { ProductRepository } from '@app/model/product.repository';
+import { STORE_PAGE_CONFIG } from './store.const';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
     selector: 'store',
     // moduleId: module.id,
-    templateUrl: 'store.component.html'
+    templateUrl: 'store.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class StoreComponent {
-    constructor(private repository: ProductRepository) {}
+export class StoreComponent implements OnInit {
+    public selectedCategory: string | null = null;
+    public productsPerPage = STORE_PAGE_CONFIG.productsPerPage;
+    public selectedPage = 1;
+    public productsList = [] as Product[];
+    public productCategoriesList: string[] = [];
+
+    constructor(private repository: ProductRepository, protected cdr: ChangeDetectorRef) {}
+
+    getProducts(): void {
+        const pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+        this.productsList = this.repository.getProducts(this.selectedCategory).slice(pageIndex, pageIndex + this.productsPerPage);
+    }
+
+    getCategories(): void {
+        this.productCategoriesList = this.repository.getCategories();
+    }
 
     get products(): Product[] {
-        return this.repository.getProducts();
+        return this.productsList;
     }
 
     get categories(): string[] {
-        return this.repository.getCategories();
+        return this.productCategoriesList;
+    }
+
+    changeCategory(newCategory: string | null = null): void {
+        this.selectedCategory = newCategory;
+        this.getProducts();
+        this.cdr.detectChanges();
+    }
+
+    changePage(newPage: number): void {
+        this.selectedPage = newPage;
+    }
+
+    changePageSize(newSize: number): void {
+        this.productsPerPage = newSize;
+        this.changePage(1);
+    }
+
+    get pageNumbers(): number[] {
+        return Array(Math.ceil(this.productsList.length / this.productsPerPage)).fill(0).map((item, index) => index + 1);
+    }
+
+    ngOnInit(): void {
+        this.productCategoriesList = this.repository.getCategories();
+        this.getProducts();
+        this.getCategories();
     }
 }
---
+++ src/app/store/store.const.ts
@@ -0,0 +1,3 @@
+export const STORE_PAGE_CONFIG = {
+    productsPerPage: 4
+};
